0a0af0c0ecb7001c215041d70283cd1205406895
Remdd <munkrat@gmail.com>
2018-10-29 22:40:54 +0000

    Further develop main menu

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ function mainMenu() {
 	stdout.write("Enter '0' to exit.\n\n");
 	//	Get user choice
 	let choice = rls.question('What do you want to do?   >   ');
-	stdout.write(choice);
+	stdout.write('\n');
 	switch(choice) {
 		case '1':
 			addNewGallery();
@@ -45,15 +45,12 @@ function addNewGallery() {
 	stdout.write("\nFirst you must give your new gallery a unique folder name.\n");
 	stdout.write("Folder names must not include spaces, but should be descriptive - for example 'AnnaVonHauswolff'.\n");
 	//	Get new folder name
-	// let galleryObj = {};
-	var folderName = getFolderName();
-	var displayName = getDisplayName(folderName);
-	var folderPath = path.join(__dirname, folderName);
-	createFolder(folderName, folderPath, displayName, function() {
-		readJpgsInFolder(folderName, folderPath, function() {
-			stdout.write('ASDASASDASDASDASD');
-		});
-	});
+	var galleryObj = {};
+	while(!galleryObj.folderName) {
+		galleryObj.folderName = getFolderName();
+	}
+	galleryObj.folderPath = path.join(__dirname, galleryObj.folderName);
+	createFolder(galleryObj);
 }
 
 function enterToContinue() {
@@ -61,14 +58,13 @@ function enterToContinue() {
 }
 
 function getFolderName() {
-	var folderName = '';
-	folderName = rls.question('\nEnter your new folder name:   >   ');
+	let folderName = rls.question('\nEnter your new folder name:   >   ');
 	if(folderName.length === 0) {
 		stdout.write('\nYou must provide a folder name!\n');
-		getFolderName();
+		return false;
 	} else if(!folderNameRegex.test(folderName)) {
 		stdout.write('\nFolder names can only contain alphanumeric characters and - or _ symbols, with no spaces.\n');
-		getFolderName();
+		return false;
 	} else {
 		return folderName;
 	}
@@ -78,22 +74,22 @@ function getDisplayName(folderName) {
 	let displayName = rls.question(`\nEnter a full display name for the "${folderName}" gallery:   >   `);
 	if(displayName.length === 0) {
 		stdout.write('\nYou must give the gallery a display name!\n');
-		getDisplayName(folderName);
+		return false;
 	} else {
 		return displayName;
 	}
 }
 
-function createFolder(folderName, folderPath, displayName, callback) {
-	stdout.write(`\nCreating folder: "${folderPath}"\n`);
-	if(fs.existsSync(folderPath)) {
+function createFolder(galleryObj) {
+	stdout.write(`\nCreating folder: "${galleryObj.folderPath}"\n`);
+	if(fs.existsSync(galleryObj.folderPath)) {
 		stdout.write("\nThat folder already exists!\n");
-		stdout.write(`Do you want to edit the existing "${folderName}" gallery, or create a new gallery with a different name?\n`);
+		stdout.write(`Do you want to edit the existing "${galleryObj.folderName}" gallery, or create a new gallery with a different name?\n`);
 		stdout.write('Enter "1" to edit the existing gallery, or 2 to choose a different name:   >   ');
 		let choice = getChoice(2);
 		switch(choice) {
 			case '1':
-				editGallery(folderName);
+				editGallery(galleryObj.folderName);
 				break;
 			case '2':
 				addNewGallery();
@@ -101,26 +97,30 @@ function createFolder(folderName, folderPath, displayName, callback) {
 			default:
 				stdout.write('Error!');
 				process.exit(0);
+				break;
 		}
 	} else {
-		stdout.write(folderPath);
-		fs.mkdir(folderPath, (err) => {
+		stdout.write(galleryObj.folderPath);
+		fs.mkdir(galleryObj.folderPath, (err) => {
 			if(err) {
 				stdout.write("\nThat folder name is not valid!\n");
 				addNewGallery();
 			} else {
 				stdout.write('\n...creating "thumbs" subfolder...\n');
-				fs.mkdir(path.join(folderPath, 'thumbs'), (err) => {
+				fs.mkdir(path.join(galleryObj.folderPath, 'thumbs'), (err) => {
 					stdout.write('\n..."thumbs" subfolder created...');
 					if(err) {
 						stdout.write(err);
 						throw(err);				
 					} else {
-						stdout.write(`\n"${folderName}" folder successfully created at "${folderPath}".\n`);
+						stdout.write(`\n"${galleryObj.folderName}" folder successfully created at "${galleryObj.folderPath}".\n`);
+						while(!galleryObj.displayName) {
+							galleryObj.displayName = getDisplayName(galleryObj.folderName);
+						}
 						stdout.write("Before continuing, please copy the photos to be displayed in this new gallery into this new folder.\n");
 						stdout.write("Photos must be in '.jpg' format.\n");
 						enterToContinue();
-						callback();
+						readJpgsInFolder(galleryObj);
 					}
 				});
 			}
@@ -128,25 +128,25 @@ function createFolder(folderName, folderPath, displayName, callback) {
 	}
 }
 
-function readJpgsInFolder(folderName, folderPath) {
-	fs.readdir(folderPath, (err, files) => {
+function readJpgsInFolder(galleryObj) {
+	fs.readdir(galleryObj.folderPath, (err, files) => {
 		if(err) {
 			stdout.write(err);
 			throw(err);
 		} else {
-			var jpgFiles = [];
+			galleryObj.rawJpgFiles = [];
 			files.forEach((file) => {
 				if(file.slice(-4).toLowerCase() === '.jpg') {
-					jpgFiles.push(file);
+					galleryObj.rawJpgFiles.push(file);
 				}
 			});
-			if(jpgFiles.length === 0) {
-				stdout.write(`\nNo jpg images were detected in the "${folderName}" folder.\n`);
+			if(galleryObj.rawJpgFiles.length === 0) {
+				stdout.write(`\nNo jpg images were detected in the "${galleryObj.folderName}" folder.\n`);
 				stdout.write(`\nPress '1' to try again, or '2' to return to the main menu:   >   `);
 				choice = getChoice(2);
 				switch(choice) {
 					case '1':
-						readJpgsInFolder(folderName, folderPath);
+						readJpgsInFolder(galleryObj);
 						break;
 					case '2':
 						mainMenu();
@@ -154,9 +154,10 @@ function readJpgsInFolder(folderName, folderPath) {
 					default:
 						stdout.write('Error!');
 						process.exit(0);
+						break;
 				}
 			} else {
-				processJpgs(folderName, folderPath, jpgFiles);
+				processJpgs(galleryObj);
 			}
 		}
 	});
@@ -164,7 +165,7 @@ function readJpgsInFolder(folderName, folderPath) {
 
 function getChoice(numberOfOptions) {
 	let choice = rls.question('');
-	if(choice > 0 && choice <= numberOfOptions) {
+	if(choice.isInteger() && choice > 0 && choice <= numberOfOptions) {
 		return choice;
 	} else {
 		stdout.write("\nChoice not recognised - please try again:   >   ");
@@ -172,47 +173,51 @@ function getChoice(numberOfOptions) {
 	}
 }
 
-function processJpgs(folderName, folderPath, jpgFiles) {
+function processJpgs(galleryObj) {
 	var output = cp.execSync('magick -version');
-	stdout.write(output.toString('utf8'));
+	// stdout.write(output.toString('utf8'));
 	if(output.toString('utf8').slice(0,7) != 'Version') {
 		stdout.write("ImageMagick is not installed!");
+		enterToContinue();
 		mainMenu();
 	} else {
-		var newFiles = [];
-		jpgFiles.forEach((jpgFile, index) => {
-			let newFileName = folderName + '_' + index + '.jpg';
-			let newFilePath = path.join(folderPath, newFileName);
-			fs.renameSync(path.join(folderPath, jpgFile), newFilePath);
+		galleryObj.jpgFiles = [];
+		galleryObj.rawJpgFiles.forEach((jpgFile, index) => {
+			let newFileName = galleryObj.folderName + '_' + index + '.jpg';
+			let newFilePath = path.join(galleryObj.folderPath, newFileName);
+			fs.renameSync(path.join(galleryObj.folderPath, jpgFile), newFilePath);
 			cp.execSync('magick mogrify -geometry 1250x1250 ' + newFilePath);
-			fs.copyFileSync(newFilePath, path.join(folderPath, 'thumbs', newFileName));
-			cp.execSync('magick mogrify -geometry 400x ' + path.join(folderPath, 'thumbs', newFileName));
-			newFiles.push(newFileName);
+			fs.copyFileSync(newFilePath, path.join(galleryObj.folderPath, 'thumbs', newFileName));
+			cp.execSync('magick mogrify -geometry 400x ' + path.join(galleryObj.folderPath, 'thumbs', newFileName));
+			galleryObj.jpgFiles.push(newFileName);
 		});
-		initializeXml(folderName, folderPath, newFiles);
+		delete galleryObj.rawJpgFiles;
+		initializeXml(galleryObj);
 	}
 }
 
-function initializeXml(folderName, folderPath, jpgFiles, fullName) {
-	let xmlFile = folderName + '.xml';
+function initializeXml(galleryObj) {
+	let xmlFile = galleryObj.folderName + '.xml';
 	var content = '<?xml version="1.0" encoding="UTF-8"?>\n' + '<document>\n' +
-		`\t<gallery folder="${folderName}" fullname="${fullName}">\n`;
-	jpgFiles.forEach((fileName, index) => {
+		`\t<gallery folderName="${galleryObj.folderName}" displayName="${galleryObj.displayName}">\n`;
+	galleryObj.jpgFiles.forEach((fileName, index) => {
 		content += `\t\t<photo position="${index}">${fileName}</photo>\n`;
 	});
 	content += '\t</gallery>\n' + '</document>\n';
 
-	fs.writeFile(path.join(folderPath, xmlFile), content, (err) => {
+	fs.writeFile(path.join(galleryObj.folderPath, xmlFile), content, (err) => {
 		if(err) {
 			stdout.write(err);
 			throw(err);
 		} else {
-			stdout.write("The XML file has been saved!");
+			stdout.write(`\nThe "${galleryObj.displayName}" gallery has been created with ${galleryObj.jpgFiles.length} photos!\n`);
+			enterToContinue();
+			mainMenu();
 		}
 	})
 }
 
-function editGallery(galleryName) {
+function editGallery(folderName) {
 	stdout.write('\nEditing existing gallery!\n');
 	mainMenu();
 }
