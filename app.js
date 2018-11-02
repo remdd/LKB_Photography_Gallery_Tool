d32bb7d7425b313deb325ff2cf8bc823a939260e
Remdd <munkrat@gmail.com>
2018-11-02 22:55:16 +0000

    Further development of basic menu

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 var rls 						= require('readline-sync'),
-		rl 							=	require('readline'),
 		fs 							= require('fs'),
 		util 						=	require('util'),
 		cp 							=	require('child_process'),
@@ -20,8 +19,6 @@ app.use(express.static(publicFolder));
 
 var folderNameRegex = RegExp(/^[a-zA-Z0-9-_]{0,20}$/);
 
-
-
 function mainMenu() {
 	stdout.write("\n\n----------------------------------------------\n");
 	stdout.write("Welcome to the LKB Photography gallery editor!\n");
@@ -29,42 +26,52 @@ function mainMenu() {
 	stdout.write("Enter '1' to create a new gallery.\n");
 	stdout.write("Enter '2' to edit an existing gallery.\n");
 	stdout.write("Enter '3' to edit the photos displayed on the homepage.\n");
-	stdout.write("Enter '0' to exit.\n\n");
+	stdout.write("Enter '4' to exit.\n\n");
 	//	Get user choice
-	let choice = rls.question('What do you want to do?   >   ');
+	var choice = getChoice(4);
 	switch(choice) {
-		case '1':
+		case 1:
 			addNewGallery();
 			break;
-		case '2':
+		case 2:
 			editGallery();
 			break;
-		case '3':
+		case 3:
 			editHomepage();
 			break;
-		case '0':
+		case 4:
 			exitApp();
 			break;
 		default:
-			stdout.write("\n\nOption not recognised - please try again.\n\n");
-			mainMenu();
+			throw 'Invalid choice!';
 			break;
 	}
 }
 
+function getChoice(numberOfOptions, message) {
+	var input;
+	if(message) {
+		input = parseInt(rls.question('\n' + message));
+	} else {
+		input = parseInt(rls.question('\nEnter your choice: '));
+	}
+	while(!Number.isInteger(input) || input <= 0 || input > numberOfOptions) {
+		input = parseInt(rls.question('\nChoice not recognised! Please try again: '));
+	}
+	return input;
+}
+
 function addNewGallery() {
-	stdout.write("\nFirst you must give your new gallery a unique folder name.\n");
+	stdout.write("\nPlease give your new gallery a unique folder name.\n");
 	stdout.write("Folder names must not include spaces, but should be descriptive - for example 'AnnaVonHauswolff'.\n");
 	//	Get new folder name
-	// let galleryObj = {};
-	var folderName = getFolderName();
-	var displayName = getDisplayName(folderName);
-	var folderPath = path.join(__dirname, publicFolder, galleryFolder, folderName);
-	createFolder(folderName, folderPath, displayName, function() {
-		readJpgsInFolder(folderName, folderPath, function() {
-			stdout.write('ASDASASDASDASDASD');
-		});
-	});
+	var galleryObj = {};
+	while(!galleryObj.folderName) {
+		galleryObj.folderName = getFolderName();
+	}
+	stdout.write(JSON.stringify(galleryObj));
+	galleryObj.folderPath = path.join(__dirname, publicFolder, galleryFolder, galleryObj.folderName);
+	createFolder(galleryObj);
 }
 
 function enterToContinue() {
@@ -72,66 +79,66 @@ function enterToContinue() {
 }
 
 function getFolderName() {
-	var folderName = '';
-	folderName = rls.question('\nEnter your new folder name:   >   ');
+	let folderName = rls.question('\nEnter your new folder name: ');
 	if(folderName.length === 0) {
 		stdout.write('\nYou must provide a folder name!\n');
-		getFolderName();
+		return false;
 	} else if(!folderNameRegex.test(folderName)) {
 		stdout.write('\nFolder names can only contain alphanumeric characters and - or _ symbols, with no spaces.\n');
-		getFolderName();
+		return false;
 	} else {
 		return folderName;
 	}
 }
 
 function getDisplayName(folderName) {
-	let displayName = rls.question(`\nEnter a full display name for the "${folderName}" gallery:   >   `);
+	let displayName = rls.question(`\nEnter a full display name for the "${folderName}" gallery: `);
 	if(displayName.length === 0) {
 		stdout.write('\nYou must give the gallery a display name!\n');
-		getDisplayName(folderName);
+		return false;
 	} else {
 		return displayName;
 	}
 }
 
-function createFolder(folderName, folderPath, displayName, callback) {
-	stdout.write(`\nCreating folder: "${folderPath}"\n`);
-	if(fs.existsSync(folderPath)) {
+function createFolder(galleryObj) {
+	stdout.write(`\nCreating folder: "${galleryObj.folderPath}"\n`);
+	if(fs.existsSync(galleryObj.folderPath)) {
 		stdout.write("\nThat folder already exists!\n");
-		stdout.write(`Do you want to edit the existing "${folderName}" gallery, or create a new gallery with a different name?\n`);
-		stdout.write('Enter "1" to edit the existing gallery, or 2 to choose a different name:   >   ');
-		let choice = getChoice(2);
+		stdout.write(`Do you want to edit the existing "${galleryObj.folderName}" gallery, or create a new gallery with a different name?\n`);
+		var choice = getChoice(2, "Enter '1' to edit the existing gallery, or '2' to choose a different name: ");
 		switch(choice) {
-			case '1':
-				editGallery(folderName);
+			case 1:
+				editGallery(galleryObj.folderName);
 				break;
-			case '2':
+			case 2:
 				addNewGallery();
 				break;
 			default:
-				stdout.write('Error!');
-				process.exit(0);
+				throw 'Invalid choice!';
+				break;
 		}
 	} else {
-		stdout.write(folderPath);
-		fs.mkdir(folderPath, (err) => {
+		stdout.write(galleryObj.folderPath);
+		fs.mkdir(galleryObj.folderPath, (err) => {
 			if(err) {
 				stdout.write("\nThat folder name is not valid!\n");
 				addNewGallery();
 			} else {
 				stdout.write('\n...creating "thumbs" subfolder...\n');
-				fs.mkdir(path.join(folderPath, 'thumbs'), (err) => {
+				fs.mkdir(path.join(galleryObj.folderPath, 'thumbs'), (err) => {
 					stdout.write('\n..."thumbs" subfolder created...');
 					if(err) {
-						stdout.write(err);
-						throw(err);				
+						throw err;
 					} else {
-						stdout.write(`\n"${folderName}" folder successfully created at "${folderPath}".\n`);
+						stdout.write(`\n"${galleryObj.folderName}" folder successfully created at "${galleryObj.folderPath}".\n`);
+						while(!galleryObj.displayName) {
+							galleryObj.displayName = getDisplayName(galleryObj.folderName);
+						}
 						stdout.write("Before continuing, please copy the photos to be displayed in this new gallery into this new folder.\n");
 						stdout.write("Photos must be in '.jpg' format.\n");
 						enterToContinue();
-						callback();
+						readJpgsInFolder(galleryObj);
 					}
 				});
 			}
@@ -139,51 +146,40 @@ function createFolder(folderName, folderPath, displayName, callback) {
 	}
 }
 
-function readJpgsInFolder(folderName, folderPath) {
-	fs.readdir(folderPath, (err, files) => {
+function readJpgsInFolder(galleryObj) {
+	fs.readdir(galleryObj.folderPath, (err, files) => {
 		if(err) {
 			stdout.write(err);
 			throw(err);
 		} else {
-			var jpgFiles = [];
+			galleryObj.rawJpgFiles = [];
 			files.forEach((file) => {
 				if(file.slice(-4).toLowerCase() === '.jpg') {
-					jpgFiles.push(file);
+					galleryObj.rawJpgFiles.push(file);
 				}
 			});
-			if(jpgFiles.length === 0) {
-				stdout.write(`\nNo jpg images were detected in the "${publicFolder}/${galleryFolder}/${folderName}" folder.\n`);
-				stdout.write(`\nPress '1' to try again, or '2' to return to the main menu:   >   `);
-				choice = getChoice(2);
+			if(galleryObj.rawJpgFiles.length === 0) {
+				stdout.write(`\nNo jpg images were detected in the "${galleryObj.folderName}" folder.\n`);
+				choice = getChoice(2, "Press '1' to try again, or '2' to return to the main menu: ");
 				switch(choice) {
-					case '1':
-						readJpgsInFolder(folderName, folderPath);
+					case 1:
+						readJpgsInFolder(galleryObj);
 						break;
-					case '2':
+					case 2:
 						mainMenu();
 						break;
 					default:
-						stdout.write('Error!');
-						process.exit(0);
+						throw 'Invalid choice!';
+						break;
 				}
 			} else {
-				processJpgs(folderName, folderPath, jpgFiles);
+				processJpgs(galleryObj);
 			}
 		}
 	});
 }
 
-function getChoice(numberOfOptions) {
-	let choice = rls.question('');
-	if(choice > 0 && choice <= numberOfOptions) {
-		return choice;
-	} else {
-		stdout.write("\nChoice not recognised - please try again:   >   ");
-		getChoice(numberOfOptions);
-	}
-}
-
-function processJpgs(folderName, folderPath, jpgFiles) {
+function processJpgs(galleryObj) {
 	console.log(process.platform);
 	var output;
 	if(process.platform === 'win32') {
@@ -196,50 +192,50 @@ function processJpgs(folderName, folderPath, jpgFiles) {
 		stdout.write("ImageMagick is not installed!");
 		mainMenu();
 	} else {
-		var newFiles = [];
-		jpgFiles.forEach((jpgFile, index) => {
-			let newFileName = folderName + '_' + index + '.jpg';
-			let newFilePath = path.join(folderPath, newFileName);
-			fs.renameSync(path.join(folderPath, jpgFile), newFilePath);
+		galleryObj.jpgFiles = [];
+		galleryObj.rawJpgFiles.forEach((jpgFile, index) => {
+			let newFileName = galleryObj.folderName + '_' + index + '.jpg';
+			let newFilePath = path.join(galleryObj.folderPath, newFileName);
+			fs.renameSync(path.join(galleryObj.folderPath, jpgFile), newFilePath);
 			if(process.platform === 'win32') {
 				cp.execSync('magick mogrify -geometry 1250x1250 ' + newFilePath);
 			} else if(process.platform === 'linux') {
 				cp.execSync('mogrify -geometry 1250x1250 ' + newFilePath);
 			}
-			fs.copyFileSync(newFilePath, path.join(folderPath, 'thumbs', newFileName));
+			fs.copyFileSync(newFilePath, path.join(galleryObj.folderPath, 'thumbs', newFileName));
 			if(process.platform === 'win32') {
-				cp.execSync('magick mogrify -geometry 400x ' + path.join(folderPath, 'thumbs', newFileName));
+				cp.execSync('magick mogrify -geometry 400x ' + path.join(galleryObj.folderPath, 'thumbs', newFileName));
 			} else if(process.platform === 'linux') {
-				cp.execSync('mogrify -geometry 400x ' + path.join(folderPath, 'thumbs', newFileName));
+				cp.execSync('mogrify -geometry 400x ' + path.join(galleryObj.folderPath, 'thumbs', newFileName));
 			}
-			newFiles.push(newFileName);
+			galleryObj.jpgFiles.push(newFileName);
 		});
-		stdout.write(`\nImages in "${folderName}" have been formatted and thumbnails have been created.\n`);
-		initializeXml(folderName, folderPath, newFiles);
+		stdout.write(`\nImages in "${galleryObj.folderName}" have been formatted and thumbnails have been created.\n`);
+		initializeXml(galleryObj);
 	}
 }
 
-function initializeXml(folderName, folderPath, jpgFiles, fullName) {
-	let xmlFile = folderName + '.xml';
+function initializeXml(galleryObj) {
+	let xmlFile = galleryObj.folderName + '.xml';
 	var content = '<?xml version="1.0" encoding="UTF-8"?>\n' + '<document>\n' +
-		`\t<gallery folder="${folderName}" fullname="${fullName}">\n`;
-	jpgFiles.forEach((fileName, index) => {
+		`\t<gallery folder="${galleryObj.folderName}" fullname="${galleryObj.fullName}">\n`;
+	galleryObj.jpgFiles.forEach((fileName, index) => {
 		content += `\t\t<photo position="${index}">${fileName}</photo>\n`;
 	});
 	content += '\t</gallery>\n' + '</document>\n';
 
-	fs.writeFile(path.join(folderPath, xmlFile), content, (err) => {
+	fs.writeFile(path.join(galleryObj.folderPath, xmlFile), content, (err) => {
 		if(err) {
 			stdout.write(err);
 			throw(err);
 		} else {
-			stdout.write(`\nAn XML file has been initialized for the "${folderName}" gallery.\n`);
-			stdout.write(`Press '1' to view and edit layout of the "${folderName}" gallery, or '2' to return to the main menu.\n`);
+			stdout.write(`\nAn XML file has been initialized for the "${galleryObj.folderName}" gallery.\n`);
+			stdout.write(`Press '1' to view and edit layout of the "${galleryObj.folderName}" gallery, or '2' to return to the main menu.\n`);
 			let choice = rls.question('What do you want to do?   >   ');
 			stdout.write(choice);
 			switch(choice) {
 				case '1':
-					editGallery(folderName);
+					editGallery(galleryObj.folderName);
 					break;
 				case '2':
 					mainMenu();
@@ -263,8 +259,9 @@ function editGallery(galleryName) {
 		stdout.write(`\nEnter the number of the gallery to edit:   >   `);
 		let choice = getChoice(galleryNames.length);
 		galleryName = galleryNames[choice - 1];
-		stdout.write(`\nEditing "${galleryName}" gallery...`);
 	}
+	stdout.write(`\nEditing "${galleryName}" gallery...`);
+	enterToContinue();
 	mainMenu();
 }
 
@@ -285,16 +282,13 @@ function editHomepage() {
 }
 
 function exitApp() {
-	stdout.write('\nBye!\n\n')
+	stdout.write('Bye!\n\n')
 	process.exit(0);
 }
-
-
 
 function startServer() {
 
 }
-
 
 
 mainMenu();
