faa59369419f028cbb2fd6cb3d62d38023f630e6
Remdd <munkrat@gmail.com>
2018-12-16 21:18:29 +0000

    Reorganise folders and add basic site dependencies

--- website/index.js
+++ website/index.js
@@ -3,6 +3,11 @@ var express 				= require('express'),
 	dotenv						= require('dotenv'),
 	favicon						= require('serve-favicon'),
 	bodyParser				= require('body-parser'),
+	opn								=	require('opn'),
+	del								=	require('del'),
+	path							=	require('path'),
+	fs								=	require('fs-extra'),
+	util							=	require('util'),
 	app 							= express();
 
 app.set('view engine', 'ejs');
@@ -12,6 +17,16 @@ app.use(logger('dev'));
 app.use(bodyParser.urlencoded({extended: true}));
 
 dotenv.config({path: '.env'});				//	Loads environment variables file
+
+const lkb = {
+	path: {
+		root: __dirname,
+		public: 'public',
+		galleries: 'galleries'
+	}
+}
+lkb.path.full = path.join(lkb.path.root, lkb.path.public, lkb.path.galleries);
+
 
 //	ROUTES	//
 app.get('/', function(req, res) {
@@ -23,6 +38,35 @@ app.get('/g', (req, res) => {
 	res.render('gallery', {gallery: gallery});
 });
 
+
+function loadGalleryNames() {
+	lkb.galleries = [];
+	let galleries = fs.readdirSync(lkb.path.full).map(name => path.join(lkb.path.full, name)).filter(checkIfFolder);
+		galleries.forEach(gallery => {
+			lkb.galleries.push(path.basename(gallery).toLowerCase());
+		});
+}
+
+function checkIfFolder(target) {
+	return fs.lstatSync(target).isDirectory();
+}
+
+function loadGalleryXml(galleryName) {
+	let galleryObj = {};
+
+}
+
+
+function setup() {
+	loadGalleryNames();
+	console.log(lkb.galleries);
+}
+
+
+
+
+//	Start server
 app.listen(process.env.PORT, process.env.IP, function() {
+	setup();
 	console.log("Server started");
 });
